a44c5f8d2e857311fd5c6f058719458a55ad6071
devltt404 <phamductri4862@gmail.com>
2022-12-12 14:57:30 +0700

    last

--- controllers/getAnswers.js
+++ controllers/getAnswers.js
@@ -13,19 +13,17 @@ module.exports = {
 
   async postAnswers(req, res) {
     let input;
-
-    if (true) {
+    const inputStr = req.body.input;
+    if (inputStr.slice(0, 4) === "http") {
       try {
-        let { body: result } = await got(
-          "https://byzantium-marlin-garb.cyclic.app/fakeWeb"
-        );
+        let { body: result } = await got(inputStr);
         result = result.replace(/(\r\n|\n|\r)/gm, "");
         result = result.replace(/\s/g, "");
         const startSplit = result.indexOf(`data="`) + 6;
         result = result.slice(startSplit, result.indexOf(`";`, startSplit));
         input = Buffer.from(result, "base64").toString("utf-8");
       } catch (error) {
-        throw new Error(error);
+        res.send("Cannot access the url");
       }
     } else {
       input = Buffer.from(req.body.input, "base64").toString("utf-8");
@@ -61,39 +59,5 @@ module.exports = {
       pageTitle: "Get Answers",
       result,
     });
-    // for (let i = 1; i < splitMul.length; i++) {
-    //   fs.appendFileSync(
-    //     "src\\file\\split.html",
-    //     `${splitMul[i]}
-    // ---------------------------------------------------------------------------------------------------------------------------------------------\n`,
-    //     function (err) {}
-    //   );
-    //   let from = 0;
-    //   from = splitMul[i].indexOf('["', from) + 2;
-    //   let temp = `${splitMul[i].slice(
-    //     from,
-    //     splitMul[i].indexOf('"]', from)
-    //   )}\n`;
-    //   for (let j = 0; j < 4; j++) {
-    //     from = splitMul[i].indexOf('["', from) + 2;
-    //     let ans = alpha[j] + ". ";
-    //     ans += splitMul[i].slice(from, splitMul[i].indexOf('"]', from));
-    //     from = splitMul[i].indexOf('"c":', from) + 1;
-    //     from = splitMul[i].indexOf('"c":', from) + 4;
-    //     if (splitMul[i][from] == "t") ans = ans + " <= ĐÚNG";
-    //     temp += `${ans}\n`;
-    //   }
-    //   temp += "\n";
-    //   str.push(temp);
-    // }
-    // function sortComparer(a, b) {
-    //   return a.localeCompare(b);
-    // }
-    // str.sort(sortComparer);
-    // let stt = 71;
-    // for (let i = 0; i < str.length; i++) {
-    //   appFile(`${++stt}. ${str[i]}`);
-    // }
-    // open("src\\file\\multipleChoice.txt");
   },
 };
